eb2b4a189ed3a01cafc6d109b610c04be0d6a5f5
Manuel Sauceda <manuel.sauceda21@gmail.com>
2020-03-28 16:14:34 -0600

    Course updates

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { CardList } from './components/card-list/card-list.component';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super(); 
+
+    this.state = {
+      monsters: [],
+      searchField: ''
+    };
+  }
+
+  componentDidMount() {
+    // Fetch the monsters from the api
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
+  }
+
+  render() {
+    //Destructure mode for assigning copies of monsters and searchField
+    const { monsters, searchField } = this.state;
+
+    //Filter the monsters before we render the component
+    const filteredMonsters = monsters.filter(monster => {
+      return monster.name.toLowerCase()
+        .includes(searchField.toLowerCase());
+    });
+
+    return (
+      <div className="App">
+        {/* Search Bar */}
+        <input 
+          type="search" 
+          placeholder="Search monsters" 
+          onChange={e => this.setState({ searchField: e.target.value })}
+        />
+
+        {/* CardList Component */}
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ monsters-rolodex/src/components/card-list/card-list.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Card } from '../card/card.component';
+import './card-list.style.css'
+
+export const CardList = (props) => (
+    <div className="card-list">
+        {
+            //Iterate through each of the monsters passed in the props and create a new Card Component
+            props.monsters.map(monster => (
+                //Key shoulld always be set for a iteration
+                <Card key={monster.id} monster={monster} />
+            ))
+        }
+    </div>
+)
