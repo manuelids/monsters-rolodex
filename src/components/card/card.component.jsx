de998eda3ee5eabbafe1c9067cbdae81560447ac
Manuel Sauceda <manuelsauc@herbalife.com>
2020-04-01 15:28:08 -0600

    Adding gh pages

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -10,7 +10,7 @@ export const Card = (props) => (
             alt="monster" 
             src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`} 
         />
-        <h1>{props.monster.name}</h1>
+        <h3>{props.monster.name}</h3>
         <p>{props.monster.email}</p>
     </div>
 )
