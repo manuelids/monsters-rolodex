97fd7ccaf539d6248847e8ca10446629de54c9b0
Manuel Sauceda <manuelsauc@herbalife.com>
2020-04-01 15:22:51 -0600

    Updates

---
+++ precache-manifest.f029bd3986d73bed16ec90f2a83f4bc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d9a6472d574ca58432e8569f697a45c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9e57e354536be438f5e0",
+    "url": "/monsters-rolodex/static/css/main.5cf4b0e9.chunk.css"
+  },
+  {
+    "revision": "147f1327e7798733efc4",
+    "url": "/monsters-rolodex/static/js/2.d562cfe4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.d562cfe4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e57e354536be438f5e0",
+    "url": "/monsters-rolodex/static/js/main.c7339b2a.chunk.js"
+  },
+  {
+    "revision": "83fef09b6b4b91e8e383",
+    "url": "/monsters-rolodex/static/js/runtime-main.6036d2a0.js"
+  }
+]);
